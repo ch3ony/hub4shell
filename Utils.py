cafb10ae378841a4f050d8903189cd75b2a34c77
kukuwhy <rla9826@naver.com>
2021-12-31 10:59:11 +0900

    util add

--- Utils.py
+++ Utils.py
@@ -2,6 +2,7 @@ import string
 import random
 import socket
 from PyInquirer import prompt
+import psutil
 
 def randomName():
     className = ''.join(random.choice(string.ascii_lowercase) for _ in range(6))
@@ -20,8 +21,9 @@ def printPrompt(name, message):
     return pmt
 
 def getAddress():
-    print(socket.gethostbyname(socket.gethostname()))
-    return
+    addrs = psutil.net_if_addrs()
+    for addr in addrs.keys() :
+        print(addrs[addr][1][1])
 
 
 if __name__ == "__main__":
